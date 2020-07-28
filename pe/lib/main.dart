1c0c104d393a7972ea40d66e4026922af085256b
Victor Hoffmann Risager <vict2464@elev.aatg.dk>
2020-07-28 20:08:26 +0200

    created a new Personal expenses app project

---
+++ pe/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter app'),
+      ),
+      body: Center(
+        child: Text('Widget playground'),
+      ),
+    );
+  }
+}
